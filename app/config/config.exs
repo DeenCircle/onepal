1bef7ce95365d8c88b503bfa7ca07b84f0911914
Ahmed AlSalali <a.s.alsalali@gmail.com>
2025-10-11 15:21:07 +0200

    added default user login

--- app/config/config.exs
+++ app/config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :onepal, :scopes,
+  user: [
+    default: true,
+    module: Onepal.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: Onepal.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :onepal,
   ecto_repos: [Onepal.Repo],
   generators: [timestamp_type: :utc_datetime]
